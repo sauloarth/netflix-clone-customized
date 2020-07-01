aa0767ef988b901e09e8f90ed802924803dd3918
Saulo Reis <sauloarth@gmail.com>
2020-07-01 14:17:15 -0300

    Configurando ci/cd para production

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 import { registerRootComponent } from 'expo';
 
 import App from './App';
+import React from 'react'
+import codePush from 'react-native-code-push'
+
+let codePushOptions = { checkFrequency: codePush.CheckFrequency.ON_APP_RESUME }
+
+Myapp = codePush(codePushOptions)(App)
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in the Expo client or in a native build,
 // the environment is set up appropriately
-registerRootComponent(App);
+registerRootComponent(Myapp);
